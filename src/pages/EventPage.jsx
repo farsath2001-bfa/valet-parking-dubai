abe60d6267d586d6221e46d184d40d8b22457f07
Farsath <farsath2001@gmail.com>
2026-09-01 10:53:59 +0400

    update contact Number

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -133,7 +133,7 @@ export default function EventsPage() {
           </div>
           <div className="events__book-ctas">
             
-              <a href="https://wa.me/971568369999"
+              <a href="https://wa.me/971568367272"
               target="_blank"
               rel="noreferrer"
               className="btn btn--primary"
@@ -142,7 +142,7 @@ export default function EventsPage() {
               Get Event Quote
             </a>
             
-             <a href="tel:+971568369999"
+             <a href="tel:+971568367272"
               className="btn btn--outline"
             >
               Call Us Now
